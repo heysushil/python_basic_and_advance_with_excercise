49b355b2cec92d3f109f3f594a1c04934788a211
heysushil <sushil.chaudhary111@gmail.com>
2020-05-08 21:51:07 +0530

    पाइथान intorduction

---
+++ 1.basic/1.introduction.py
@@ -0,0 +1,76 @@
+# WELCOME TO HEY KYA KARU. DON'T FORGET TO SUBSCRIBE CHANEL TO GET NEW UPDATES
+# hash ke sath hum single line comment dete hai
+'''
+ye mltiline
+comment
+hai
+'''
+# print() = ye small bracke jab bhi use ho iska matlab hai ki ye ek function hai.
+# agar function pahle se bana hua hai to ye predefined function hota hai
+
+print('\n\nHello welcome to hey kya karu')
+
+# file handling
+'''
+jaise hamere system me hum ek file ke sath
+new file creat 
+existing file to rename
+existing file to delte
+existing file to move
+file read
+file me change karn
+'''
+
+'''
+Python Database Handling (CRUD)
+
+create = ya to database creat ya table
+read = use select query
+update = modify existing data
+delete = existing data ko delete karna
+'''
+
+'''
+What is Python?
+
+Python is a popular programming language. It was created by Guido van Rossum, and released in 1991.
+
+It is used for:
+
+web development (server-side),
+software development,
+mathematics,
+system scripting.
+
+
+What can Python do?
+
+Python can be used on a server to create web applications.
+Python can be used alongside software to create workflows.
+Python can connect to database systems. It can also read and modify files.
+Python can be used to handle big data and perform complex mathematics.
+Python can be used for rapid prototyping, or for production-ready software development.
+
+Why Python?
+
+Python works on different platforms (Windows, Mac, Linux, Raspberry Pi, etc).
+Python has a simple syntax similar to the English language.
+Python has syntax that allows developers to write programs with fewer lines than some other programming languages.
+Python runs on an interpreter system, meaning that code can be executed as soon as it is written. This means that prototyping can be very quick.
+Python can be treated in a procedural way, an object-orientated way or a functional way.
+Good to know
+The most recent major version of Python is Python 3, which we shall be using in this tutorial. However, Python 2, although not being updated with anything other than security updates, is still quite popular.
+In this tutorial Python will be written in a text editor. It is possible to write Python in an Integrated Development Environment, such as Thonny, Pycharm, Netbeans or Eclipse which are particularly useful when managing larger collections of Python files.
+Python Syntax compared to other programming languages
+Python was designed for readability, and has some similarities to the English language with influence from mathematics.
+Python uses new lines to complete a command, as opposed to other programming languages which often use semicolons or parentheses.
+Python relies on indentation, using whitespace, to define scope; such as the scope of loops, functions and classes. Other programming languages often use curly-brackets for this purpose.
+'''
+
+
+# syntex
+
+if 8 > 5:
+    print('Ha 4 jo hai 5 se bada hia')
+    print('Ha 4 jo hai 5 se bada hia')
+
