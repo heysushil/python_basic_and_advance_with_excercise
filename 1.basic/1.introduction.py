f5a867e738ba04e4354e52d9aaa3167e0223e2c3
heysushil <sushil.chaudhary111@gmail.com>
2020-05-08 22:49:56 +0530

    इन्ट्रोडक्शन excersie

--- 1.basic/1.introduction.py
+++ 1.basic/1.introduction.py
@@ -1,3 +1,8 @@
+# यूट्यूब के जरूरी लिंक 
+# 1. Python Basic and Advance ट्यूटोरियल प्लेलिस्ट https://www.youtube.com/playlist?list=PLK6wiPavf7QikS9PMYrGZXz1HlE1KZLD3
+# 2. PHP Projects प्लेलिस्ट https://www.youtube.com/playlist?list=PLK6wiPavf7QiEj6IPc3lkjz1wR4w9RM6B
+# 3. GitHubट्यूटोरियल प्लेलिस्ट  https://www.youtube.com/watch?v=LUyVs2MTlTM&list=PLK6wiPavf7Qjydpc5v-hdIoqCx2V19pHP
+
 # WELCOME TO HEY KYA KARU. DON'T FORGET TO SUBSCRIBE CHANEL TO GET NEW UPDATES
 # hash ke sath hum single line comment dete hai
 '''
