215c5a349bea2cbabb81e05e5f93dab182b85ef3
heysushil <sushil.chaudhary111@gmail.com>
2020-05-13 20:21:27 +0530

    पाइथान वेरीअबल कान्सेप्ट के बारें मे अपडेट

---
+++ 1.basic/2.variable.py
@@ -0,0 +1,78 @@
+# यूट्यूब के जरूरी लिंक 
+# 1. Python Basic and Advance ट्यूटोरियल प्लेलिस्ट https://www.youtube.com/playlist?list=PLK6wiPavf7QikS9PMYrGZXz1HlE1KZLD3
+# 2. PHP Projects प्लेलिस्ट https://www.youtube.com/playlist?list=PLK6wiPavf7QiEj6IPc3lkjz1wR4w9RM6B
+# 3. GitHubट्यूटोरियल प्लेलिस्ट  https://www.youtube.com/watch?v=LUyVs2MTlTM&list=PLK6wiPavf7Qjydpc5v-hdIoqCx2V19pHP
+
+# WELCOME TO HEY KYA KARU. DON'T FORGET TO SUBSCRIBE CHANEL TO GET NEW UPDATES
+
+'''
+math me hum kaise likhte hai
+
+a = 3
+b = 5
+c = a + b
+answer = 8
+
+var a = 6 c++/c
+$a = 6  php
+a = 6   python
+'''
+
+a = '5' # a ek varibale name hai aur 5 value hai jo ki a ko assign ki gai hai
+print('\n',a) # print ek predefine function jo ki pahle se hi bana hua hai. isko humen nahi baya hai
+
+'''
+varaible ko define karte time - 
+1. varibel name kabhi bhi number se nahi start hota hai
+2. varabiel ke andar kabhi bhi space ya special character nahi use karte hai
+
+
+varaibel kya hai?
+
+1. varaibal ek holder hai jo ki kisi bhi value ko hold karke rakhta hai
+2. varabile wo single value ya multiple value hold kar sakta hai.
+3. array ya object multiple values hold karte hai
+4. varabiel 2 type ke hote hai = 
+    a. local variable : local varibel ka ek specific dayra hota hai.
+    b. global variable : gloable variable har ek jagh accessable hota hai.
+
+Notes:
+    in topics ko ek baar padhlo
+        1. what is array
+        2. what is object
+        3. what is function
+'''
+
+name = 'hey kya karu'
+print('\n',name)
+
+# type() ye ek predefine function hai jo ki varaiable ke type ko bata hai.
+print('\na ka type => ',type(a))
+print('\nname ka type => ',type(name))
+
+# ek sath 1 se jada variable ko ek baar me alag alag value assign kar sakte hai
+b, c, d = 10, 20, 30
+print('\nb ki value',b)
+print('\nc ki value',c)
+print('\nd ki value',d)
+
+# hum ek sath kai variables ko ek hi value assign kar sakte hai
+e=f=g = 40
+print('\ne ki value',e)
+print('\nf ki value',f)
+print('\ng ki value',g)
+
+q = 'hello q'
+q = 'hello Q'
+
+print('\nq value = ',q)
+# print('\nQ value = ',Q)
+
+
+# contactanation
+# contactanation hamesa 2 string ke bich me hota hai agar int aur str ko concateneate karoge to error milega
+x = 'Karm karo, fal ki chinta mat karo'
+y = 10
+print('\nHey kya karu '+ y)
+
+
