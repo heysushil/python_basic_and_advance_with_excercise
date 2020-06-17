ca4e0c0c776c5ab5beb762cdc8f221ee55e66227
heysushil <sushil.chaudhary111@gmail.com>
2020-06-17 18:28:01 +0530

    array topic

--- 1.basic/18.lambda.py
+++ 1.basic/18.lambda.py
@@ -1,4 +1,6 @@
-# WELCOME TO HEY KYA KARU. DON'T FORGET TO SUBSCRIBE CHANEL TO GET NEW UPDATES
+# WELCOME TO HEY KYA KARU. DON'T FORGET TO SUBSCRIBE CHANEL TO GET NEW UPDATES - https://www.youtube.com/channel/UCphs2JfmIClR62wbyf76HDg
+
+# Contact for: College Project | Project Reporting | Documentation | Project Training | Website Development | SEO @ heykyakaru@gmail.com
 
 # मैं अनलाइन ट्रैनिंग भी देता हु तो अगर किसी को अनलाइन ट्रैनिंग करनी है तो मुझे कान्टैक्ट कर सकता है heykyakaru@gmail.com पर।
 
@@ -6,6 +8,8 @@
 # 1. Python Basic and Advance ट्यूटोरियल प्लेलिस्ट https://www.youtube.com/playlist?list=PLK6wiPavf7QikS9PMYrGZXz1HlE1KZLD3
 # 2. PHP Projects प्लेलिस्ट https://www.youtube.com/playlist?list=PLK6wiPavf7QiEj6IPc3lkjz1wR4w9RM6B
 # 3. GitHubट्यूटोरियल प्लेलिस्ट  https://www.youtube.com/watch?v=LUyVs2MTlTM&list=PLK6wiPavf7Qjydpc5v-hdIoqCx2V19pHP
+# 4. Python Project https://www.youtube.com/watch?v=3lrbbB38zpU&list=PLK6wiPavf7Qj-NLJhbkxw9QfonweHafcN
+# 5. Tips and Trick for Development: https://www.youtube.com/watch?v=vPL6ODrfcwI&list=PLK6wiPavf7QiVLYXrC2TW_fdcZp57MgMB
 
 '''
 Lambda function: 
---
+++ 1.basic/19.array.py
@@ -0,0 +1,72 @@
+# WELCOME TO HEY KYA KARU. DON'T FORGET TO SUBSCRIBE CHANEL TO GET NEW UPDATES - https://www.youtube.com/channel/UCphs2JfmIClR62wbyf76HDg
+
+# Contact for: College Project | Project Reporting | Documentation | Project Training | Website Development | SEO @ heykyakaru@gmail.com
+
+# मैं अनलाइन ट्रैनिंग भी देता हु तो अगर किसी को अनलाइन ट्रैनिंग करनी है तो मुझे कान्टैक्ट कर सकता है heykyakaru@gmail.com पर।
+
+# HEY KYA KARU यूट्यूब के जरूरी लिंक 
+# 1. Python Basic and Advance ट्यूटोरियल प्लेलिस्ट https://www.youtube.com/playlist?list=PLK6wiPavf7QikS9PMYrGZXz1HlE1KZLD3
+# 2. PHP Projects प्लेलिस्ट https://www.youtube.com/playlist?list=PLK6wiPavf7QiEj6IPc3lkjz1wR4w9RM6B
+# 3. GitHubट्यूटोरियल प्लेलिस्ट  https://www.youtube.com/watch?v=LUyVs2MTlTM&list=PLK6wiPavf7Qjydpc5v-hdIoqCx2V19pHP
+# 4. Python Project https://www.youtube.com/watch?v=3lrbbB38zpU&list=PLK6wiPavf7Qj-NLJhbkxw9QfonweHafcN
+# 5. Tips and Trick for Development: https://www.youtube.com/watch?v=vPL6ODrfcwI&list=PLK6wiPavf7QiVLYXrC2TW_fdcZp57MgMB
+
+'''
+Array ? 
+Python Library => Numpy
+
+Array:
+    1D => Dimentional => index array (similear type value store karte the) => Python List
+    2D => associative array (key=>value) => Python Dictonary
+    3D or Multi Dimentional array 
+
+List = array => index array
+Tuple
+Dict
+Set
+
+Why use array ?
+variable:
+    1. single value store
+    2. multi value store
+'''
+
+a = 1
+b = 2
+c = 3
+
+# array no 
+
+listval = [1,'python','',('Tuplevalue 1',),{'name':'Mr. Python'}]
+print('\n\nList values: ',listval)
+print('\nlist[0]: ',type(listval[0]))
+print('\nlist[1]: ',type(listval[1]))
+print('\nlist[2]: ',type(listval[2]))
+print('\nlist[3]: ',type(listval[3]))
+print('\nlist[4]: ',type(listval[4]))
+
+# find length of list
+print('\nLenght of listval: ',len(listval))
+'''
+List:
+    append() => last me new value add karne liye
+    pop() => last se ek value ko remove karne liye
+    remove() => value ke behalf pe remove karne ke liye
+    index() => list index postion ki value deta hai
+'''
+
+
+'''
+Array:
+
+searching (Dictonary)
+    FIFO => First in first out
+    LIFO => Last in first out
+
+sorting (Bubble sort / Heap sort)
+    Starting
+    End
+
+'''
+
+
