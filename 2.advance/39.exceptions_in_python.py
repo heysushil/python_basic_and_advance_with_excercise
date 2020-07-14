ddb76af1e818d68ba3ff20835a9d3e5d3e7d01e1
heysushil <sushil.chaudhary111@gmail.com>
2020-07-14 13:10:32 +0530

    all exception mehtods

---
+++ 2.advance/39.exceptions_in_python.py
@@ -0,0 +1,31 @@
+'''
+Built-in Exception:
+
+try-except/finally/else
+raise
+
+ArithmeticError: it's realted to numeric calculation
+AttributError: 
+Excetion
+FloatingPointError: flot pint calculation or range realted
+GeneratorExit: problem related to close() 
+ImportError: import module does not exists
+IndentationError: systax realted
+IndexError: string, sequesnce
+KeyError: dict key realted error
+MemoryError: 
+NameError: variable realted 
+OSError: 
+OverflowError: numearic realted
+RefernceError: object realted
+RuntimeError: 
+StropIteration: 
+SyntaxError: 
+TabError: 
+TypeError: datatype realted
+UnicoError: 
+ValveError: dict realte 
+ZeroDivisonError: 
+'''
+
+
